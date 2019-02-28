015012fe67265d142460119f7af524e91ca659b1
Anna0192 <nancyt33333@gmail.com>
2019-02-28 12:58:24 +0300

    files changed

--- index.js
+++ index.js
@@ -5,23 +5,29 @@
 module.exports = function (operations, callback) {
 
     let promises=[];
+if (operations.length ==0) { callback (null, []) }
 
+else {
     for (let i in operations) {
-        let promise = new Promise ( (resolve, reject)=> {
+        let promise = new Promise((resolve, reject) => {
             operations[i](next);
 
 
-            function next(err, data){
-                (err!=null) ?  reject(err) : resolve(data);
+            function next(err, data) {
+                (err != null) ? reject(err) : resolve(data);
             }
 
-        })
+        });
         promises.push(promise);
     }
 
-Promise.all(promises)
-       .then(function (result) {callback(null, result)})
-       .catch(function (error) {callback(error, null)})
-
+    Promise.all(promises)
+        .then(function (result) {
+            callback(null, result)
+        })
+        .catch(function (error) {
+            callback(error, null)
+        })
+}
 };
 
